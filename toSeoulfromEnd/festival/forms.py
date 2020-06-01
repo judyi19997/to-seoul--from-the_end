229fd4b41681b23aa680115f1e1e0c7b26cdbd8d
judyi19997 <judyi19997@kookmin.ac.kr>
2020-06-01 23:08:15 +0900

    festival CRUD 쌩 기본

---
+++ toSeoulfromEnd/festival/forms.py
@@ -0,0 +1,7 @@
+from .models import festival_M
+from django import forms
+
+class festival_F(forms.ModelForm):
+    class Meta():
+        model = festival_M
+        fields = ['title','locate','period','body','image','is_school']
